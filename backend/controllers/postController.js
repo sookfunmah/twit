3fcc83daa5554622d84791790a82185d8063de56
sookfunmah <sookfun.mah@gmail.com>
2024-06-16 04:43:20 +0800

    Frontend(Post.jsx) , Backend(PostController.jsx) - like/unlike post function

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -134,9 +134,8 @@ export const likeUnlikePost = async(req,res)=>{
             //unlike post
             await Post.updateOne({_id:postId}, { $pull:{ likes:userId }})
             await User.updateOne({_id:userId}, { $pull:{likedPosts:postId}})
-            // post.likes.pull(userId);  // Modified to use pull() on post object
-            //await post.save();  // Added this line to save the updated post
-            res.status(200).json({message: " Post unliked successfully"})
+            const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString())
+            res.status(200).json(updatedLikes)
         }else{
             // Like post
             post.likes.push(userId)
@@ -149,7 +148,9 @@ export const likeUnlikePost = async(req,res)=>{
                 type: "like"
             })
             await notification.save()
-            res.status(200).json({message : "Post liked successfully"})
+            
+            const updatedLikes = post.likes
+            res.status(200).json(updatedLikes)
         }
 
     } catch (error) {
--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -3,6 +3,7 @@ import { BiRepost } from "react-icons/bi";
 import { FaRegHeart } from "react-icons/fa";
 import { FaRegBookmark } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
+
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
@@ -14,7 +15,11 @@ const Post = ({ post }) => {
 	const {data:authUser} = useQuery({queryKey: ["authUser"]})
 	const queryClient = useQueryClient()
 
-	const {mutate:deletePost, isPending} = useMutation({
+
+//////////////////////////////////////
+///	DELETE POST FUNCTION
+//////////////////////////////////////
+	const {mutate:deletePost, isPending:isDeleting} = useMutation({
 		mutationFn: async() => {
 			try {
 				const res = await fetch(`/api/posts/delete/${post._id}`,{
@@ -41,9 +46,49 @@ const Post = ({ post }) => {
 		}
 	})
 
+
+////////////////////////////////////
+/// LIKE POST FUNCTION
+////////////////////////////////////
+	const {mutate:likePost, isPending:isLiking} = useMutation({
+		mutationFn: async() =>{
+			try {
+				const res = await fetch(`/api/posts/like/${post._id}`, {
+					method: "POST"
+
+				})
+				const data = await res.json()
+
+				if(!res.ok){
+					throw new Error(data.error || "Something went wrong")
+				}
+				return data
+
+			} catch (error) {
+				throw new Error(error)
+			}
+		},
+		onSuccess:(updatedLikes) =>{
+			
+			//it will refetch all post when user click like post.
+			// queryClient.invalidateQueries({queryKey:["posts"]})
+
+			queryClient.setQueryData(["posts"],(oldData) => {
+				return oldData.map((p => {
+					if(p._id === post._id) {
+						return {...p, likes:updatedLikes }
+					}
+					return p
+				}))
+			})
+		},
+		onError:(error) => {
+			toast.error(error.message)
+		}
+	})
 	
 	const postOwner = post.user;
-	const isLiked = false;
+	const isLiked = post.likes.includes(authUser._id);
 
 	const isMyPost = authUser._id === post.user._id;
 
@@ -59,7 +104,10 @@ const Post = ({ post }) => {
 		e.preventDefault();
 	};
 
-	const handleLikePost = () => {};
+	const handleLikePost = () => {
+		if(isLiking) return
+		likePost()
+	};
 
 	return (
 		<>
@@ -82,11 +130,11 @@ const Post = ({ post }) => {
 
 						{isMyPost && (
 							<span className='flex justify-end flex-1'>
-								{!isPending && (
+								{!isDeleting && (
 								<FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />
 								)}
 
-								{isPending && (
+								{isDeleting && (
 									<LoadingSpinner size = "sm"/>
 								)}
 							</span>
@@ -113,7 +161,7 @@ const Post = ({ post }) => {
 									{post.comments.length}
 								</span>
 							</div>
-							{/* We're using Modal Component from DaisyUI */}
+							{/*  Modal Component from DaisyUI */}
 							<dialog id={`comments_modal${post._id}`} className='modal border-none outline-none'>
 								<div className='modal-box rounded border border-gray-600'>
 									<h3 className='font-bold text-lg mb-4'>COMMENTS</h3>
@@ -155,11 +203,8 @@ const Post = ({ post }) => {
 											onChange={(e) => setComment(e.target.value)}
 										/>
 										<button className='btn btn-primary rounded-full btn-sm text-white px-4'>
-											{isCommenting ? (
-												<span className='loading loading-spinner loading-md'></span>
-											) : (
-												"Post"
-											)}
+											{isCommenting ? < LoadingSpinner size ='sm' />: "Post"}
+											
 										</button>
 									</form>
 								</div>
@@ -171,20 +216,24 @@ const Post = ({ post }) => {
 								<BiRepost className='w-6 h-6  text-slate-500 group-hover:text-green-500' />
 								<span className='text-sm text-slate-500 group-hover:text-green-500'>0</span>
 							</div>
+
+
 							<div className='flex gap-1 items-center group cursor-pointer' onClick={handleLikePost}>
-								{!isLiked && (
+								{isLiking && <LoadingSpinner size="sm" />}
+								{!isLiked && !isLiking &&(
 									<FaRegHeart className='w-4 h-4 cursor-pointer text-slate-500 group-hover:text-pink-500' />
 								)}
-								{isLiked && <FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
+								{isLiked && !isLiking &&<FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
 
 								<span
-									className={`text-sm text-slate-500 group-hover:text-pink-500 ${
-										isLiked ? "text-pink-500" : ""
+									className={`text-sm  group-hover:text-pink-500 ${
+										isLiked ? "text-pink-500" : "text-slate-500"
 									}`}
 								>
 									{post.likes.length}
 								</span>
 							</div>
+
 						</div>
 						<div className='flex w-1/3 justify-end gap-2 items-center'>
 							<FaRegBookmark className='w-4 h-4 text-slate-500 cursor-pointer' />
