b85e37bf9411ee0b562deb92930d8df17540fde5
sookfunmah <sookfun.mah@gmail.com>
2024-06-16 05:46:05 +0800

    frontend -Post.jsx- Comment Function, added index.js in utils

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -9,12 +9,17 @@ import { Link } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import {toast} from "react-hot-toast"
 import LoadingSpinner from "./LoadingSpinner"
+import { formatPostDate } from "../../utils/date";
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
 	const {data:authUser} = useQuery({queryKey: ["authUser"]})
 	const queryClient = useQueryClient()
 
+	const postOwner = post.user;
+	const isLiked = post.likes.includes(authUser._id);
+	const isMyPost = authUser._id === post.user._id;
+	const formattedDate = formatPostDate(post.createdAt);
 
 //////////////////////////////////////
 ///	DELETE POST FUNCTION
@@ -87,21 +92,56 @@ const Post = ({ post }) => {
 		}
 	})
 	
-	const postOwner = post.user;
-	const isLiked = post.likes.includes(authUser._id);
 
-	const isMyPost = authUser._id === post.user._id;
+////////////////////////////////////
+/// COMMENT POST FUNCTION
+////////////////////////////////////
+	const {mutate:commentPost, isPending:isCommenting} = useMutation ({
+		mutationFn : async () =>{
+			try {
+				const res = await fetch (`/api/posts/comment/${post._id}`, {
+					method: "POST",
+					headers:{
+						"Content-Type" : "application/JSON",
+					},
+					body: JSON.stringify({ text:comment })
+				})
 
-	const formattedDate = "1h";
+				const data = await res.json()
 
-	const isCommenting = false;
+				if(!res.ok){
+					throw new Error(data.error || "Somethng went wrong")
+				}
+				return data
+				
+			} catch (error) {
+				throw new Error(error)
+			}
+		},
+		onSuccess:() => {
+			//alert if success, and set the comment to empty, invalidate the query
+			toast.success("Comment posted successfully")
+			setComment("")
+			queryClient.invalidateQueries({queryKey: ["posts"]})
 
+
+		},onError:(error) => {
+			toast.error(error.message)
+		}
+	})
+
+
+/////////////////
+// Handler
+////////////////
 	const handleDeletePost = () => {
 		mutate:deletePost()
 	};
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
+		if(isCommenting) return
+		commentPost()
 	};
 
 	const handleLikePost = () => {
