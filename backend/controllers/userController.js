40b50830c4e2fa81091ab974c14f08273356c670
sookfunmah <sookfun.mah@gmail.com>
2024-06-15 21:04:33 +0800

    FrontEnd -follow hook linked to backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -50,7 +50,7 @@ export const followUnfollow = async(req,res)=>{
             //Unfollow the user
             await User.findByIdAndUpdate(id, {$pull:{followers:req.user._id}});
             await User.findByIdAndUpdate(req.user._id, { $pull :{following: id }})
-            //return the id of the user as a response
+
             res.status(200).json({message:"User unfollowed successfully"})
 
         }else{
@@ -68,7 +68,6 @@ export const followUnfollow = async(req,res)=>{
             
             await newNotification.save()
 
-            //return the id of the user as a response
             res.status(200).json({message:"User followed successfully"})
         }
 
--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
-import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-
 import { useQuery } from "@tanstack/react-query";
+
+import useFollow from "../../hooks/useFollow";
+import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
+import LoadingSpinner from "./LoadingSpinner";
 
 const RightPanel = () => {
 	
@@ -22,6 +24,8 @@ const RightPanel = () => {
 			}
 		}
 		})
+
+		const {follow, isPending} = useFollow()
 
 		//If no suggestedUser, then maintain the container size
 	if (suggestedUsers?.length === 0) {
@@ -65,9 +69,12 @@ const RightPanel = () => {
 								<div>
 									<button
 										className='btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm'
-										onClick={(e) => e.preventDefault()}
+										onClick={(e) =>{
+											e.preventDefault()
+											follow(user._id)
+										}}
 									>
-										Follow
+										{isPending ? <LoadingSpinner size="sm" /> : "Follow"}
 									</button>
 								</div>
 							</Link>
