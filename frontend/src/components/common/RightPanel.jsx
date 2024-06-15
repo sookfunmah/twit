b6151b8c0b0b01517d0f77288ea4938d7a852ce3
sookfunmah <sookfun.mah@gmail.com>
2024-06-15 20:24:24 +0800

    fix backend - add limit of image, frontend - rightPanel.jsx to fetch suggested users.

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,9 +1,32 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
+
+import { useQuery } from "@tanstack/react-query";
 
 const RightPanel = () => {
-	const isLoading = false;
+	
+	const {data:suggestedUsers, isLoading} = useQuery({
+		queryKey : ["suggestedUsers"],
+		queryFn: async () =>{
+			try {
+				const res = await fetch ("/api/users/suggested")
+				const data = await res.json()
+				
+				if(!res.ok) {
+					throw new Error (data.message || "Something went wrong")
+				}
+				return data
+
+			} catch (error) {
+				throw new Error (error.message)
+			}
+		}
+		})
+
+		//If no suggestedUser, then maintain the container size
+	if (suggestedUsers?.length === 0) {
+		return <div className="'md:w-64 w-0"></div>
+	}
 
 	return (
 		<div className='hidden lg:block my-4 mx-2'>
@@ -20,7 +43,7 @@ const RightPanel = () => {
 						</>
 					)}
 					{!isLoading &&
-						USERS_FOR_RIGHT_PANEL?.map((user) => (
+						suggestedUsers?.map((user) => (
 							<Link
 								to={`/profile/${user.username}`}
 								className='flex items-center justify-between gap-4'
@@ -34,7 +57,7 @@ const RightPanel = () => {
 									</div>
 									<div className='flex flex-col'>
 										<span className='font-semibold tracking-tight truncate w-28'>
-											{user.fullName}
+											{user.fullname}
 										</span>
 										<span className='text-sm text-slate-500'>@{user.username}</span>
 									</div>
--- frontend/src/pages/home/CreatePost.jsx
+++ frontend/src/pages/home/CreatePost.jsx
@@ -17,7 +17,7 @@ const CreatePost = () => {
 	const {data:authUser} =useQuery({queryKey: ["authUser"]})
 	const queryClient = useQueryClient()
 	const {mutate:CreatePost, isPending , isError, error} = useMutation({
-		mutationFn : async ({text,img}) =>{
+		mutationFn : async ({ text, img}) =>{
 			try {
 				const res = await fetch("/api/posts/create", {
 					method : "POST",
