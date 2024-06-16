b83c1bff7362ede55680b426de31d12c87a7914f
sookfunmah <sookfun.mah@gmail.com>
2024-06-16 20:35:49 +0800

    RightPanel -data.error change, ProfilePage - modify to remove UPDATE button after image change.

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -156,7 +156,11 @@ const ProfilePage = () => {
 								{(coverImg || profileImg) && (
 									<button
 										className='btn btn-primary rounded-full btn-sm text-white px-4 ml-2'
-										onClick={() => updateProfile({coverImg, profileImg})}
+										onClick={async () => {
+											await updateProfile({coverImg, profileImg})
+											setProfileImg(null)
+											setCoverImg(null)
+									}}
 									>
 										{isUpdatingProfile ? "Updating..." : "Update"}
 									</button>
