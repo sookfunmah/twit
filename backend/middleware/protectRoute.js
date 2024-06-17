4e469202ad998515b76b5a06d192b6bb0bdb120e
sookfunmah <sookfun.mah@gmail.com>
2024-06-17 21:52:30 +0800

    changing vercel.json and  protectRoute - trying to deploy backend

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -5,7 +5,8 @@ import jwt from "jsonwebtoken"
 export const protectRoute =async (req,res,next)=>{
     try {
         //Get token from cookies
-        const token =req.cookies.jwt
+        // const token =req.cookies.jwt
+        const token = req.cookies.jwt || req.headers.authorization?.split(' ')[1];
         if(!token){
             return res.status(401).json({error:"Unauthorized:No token Provided"})
         }
